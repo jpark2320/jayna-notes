0da0d96706bac82da8464e555f74cc2b3ac40aea
connectshops <connectshopsco@gmail.com>
2019-03-04 21:35:04 -0800

    Set up Apollo Client and global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import "./globalStyles";
+import GlobalStyle from "./globalStyles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyle />
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
